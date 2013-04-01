0881e1b0b667e46f9f1a2040bfb13216afce2b60
TJ Jaksa <tgamer27@gmail.com>
2013-04-01 12:22:56 -0400

    Check in

--- minecraft/net/progfish/worldgens/gen/WorldGenRingSphere.java
+++ minecraft/net/progfish/worldgens/gen/WorldGenRingSphere.java
@@ -3,6 +3,8 @@ package net.progfish.worldgens.gen;
 import java.util.Random;
 
 import net.minecraft.block.Block;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntityChest;
 import net.minecraft.world.World;
 
@@ -30,7 +32,36 @@ public class WorldGenRingSphere extends FishyWorldGenBase {
 		TileEntityChest chest = (TileEntityChest) worldObj.getBlockTileEntity(i, j, k);
 		if(chest != null)
 		{
-			
+			int amountOfItem = 3 + rand.nextInt(4);
+			for(int a = 0; a < amountOfItem; a++)
+			{
+				chest.setInventorySlotContents(rand.nextInt(chest.getSizeInventory()), getItem());
+			}
+		}
+	}
+	
+	private ItemStack getItem()
+	{
+		switch(rand.nextInt(10))
+		{
+			case 0:
+				return new ItemStack(Item.appleRed, rand.nextInt(5) + 2, 0);
+			case 1:
+				return new ItemStack(Item.appleGold, 1, 0);
+			case 2:
+				return new ItemStack(Item.arrow, rand.nextInt(30) + 25, 0);
+			case 3:
+				return new ItemStack(Item.saddle, 1, 0);
+			case 4:
+				return new ItemStack(Item.pocketSundial, 1, 0);
+			case 5:
+				return new ItemStack(Item.bootsChain, 1, 0);
+			case 6:
+				return new ItemStack(Item.plateChain, 1, 0);
+			case 7:
+				return new ItemStack(Item.eyeOfEnder, rand.nextInt(5) + 2, 0);
+			default:
+				return new ItemStack(Item.porkCooked, rand.nextInt(5) + 2, 0);
 		}
 	}
 
